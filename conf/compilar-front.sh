83dfdb6fb96f37affaf1a416b276313377e73076
Marco Redondo <redondomarco@gmail.com>
2025-11-14 15:04:55 -0300

    repongo versiones para compilar

--- conf/compilar-front.sh
+++ conf/compilar-front.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+#cd /
 
-cd /
-nvm install 10
-nvm use 10
-npm install -g @angular/cli
+#nvm use 20
+#npm install -g @angular/cli
 
-#
+#nvm install 10
+#nvm use 10
+
+#cd /
+#npm install -g @angular/cli
+
+#cd /gaj-frontend/src/tmf-frontend-ui/
+#npm install
+
+#ng serve --host 0.0.0.0
+
 cd /gaj-frontend/src/tmf-frontend-ui/
-npm install
-
+npm install --legacy-peer-deps
+npm install -g @angular/cli
+npm install @angular/cli
 ng serve --host 0.0.0.0
